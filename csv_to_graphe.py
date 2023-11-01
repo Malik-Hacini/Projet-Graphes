e28bb5094a92776328ec966fcb434690bfc7f35c
Concombrero <boyer.tim.travail@gmail.com>
2023-11-01 12:12:20 +0100

    Commentaire csv_to_graph

--- csv_to_graphe.py
+++ csv_to_graphe.py
@@ -6,7 +6,7 @@ class UniteError(Exception):
     pass
 
 def from_csv(nom_fichier_csv: str)->list:
-    """Programme qui ouvre un fichier csv et qui le tranforme en liste de liste mot
+    """Programme qui ouvre un fichier csv et qui le tranforme en liste de liste de mot
 
     Args:
         nom_fichier_csv (str): Nom du fichier à utiliser
@@ -27,6 +27,7 @@ def traitement_information(liste_informations, n_suivi=None)->tuple[list,set,lis
 
     Args:
         liste_informations (list): la liste des lignes d'un fichier
+        n_suivi: (int or None): Le numéro du suivi auquels on s'interresse. (None si on s'interesse qu'a la duree initiale)
 
     Returns:
         tuple: 
@@ -37,50 +38,50 @@ def traitement_information(liste_informations, n_suivi=None)->tuple[list,set,lis
                     str: le nom du noeud
                     int: son poids
     """
-    noeuds=[]
-    arcs=set()
-    poids=[]
-    for ligne in liste_informations:
-        noeud=ligne[0]
+    noeuds=[] #La liste des noeuds
+    arcs=set() #L'ensemble des arcs
+    poids=[] #La liste des poids
+    for ligne in liste_informations: #Pour chaque ligne, donc chaque noeud
+        noeud=ligne[0] #On rajoute le noeud à la liste
         noeuds.append(noeud)
-        duree_tache=ligne[2]
-        if n_suivi!=None:
-            for i in range(4,5+n_suivi):
-                if ligne[i]!='':
-                    duree_tache=ligne[i]
-        duree_tache=conversion_unite(duree_tache)
-        poids.append((noeud,duree_tache))
-        if ligne[3]!='':
-            pre_noeuds=ligne[3]
-            for pre_noeud in pre_noeuds.split():
-                arcs.add((pre_noeud, noeud))
-        if noeud=='F':
-            poids_final=duree_tache       
+        duree_tache=ligne[2] #La duree de la tache est initialement la colonne duree
+        if n_suivi!=None: #Si on s'interesse à un suivi
+            for i in range(4,5+n_suivi): #Pour chaque colonne de suivi auxquelles on s'interresse
+                if ligne[i]!='': #Si le suivi existe
+                    duree_tache=ligne[i] #La duree de la tache est celle du suivi
+        duree_tache=conversion_unite(duree_tache) #On convertit la duree qui était une valeur suivi d'une unité temporelle en nombre de jour
+        poids.append((noeud,duree_tache)) #On associe le noeud à son poids et on le rajoute dans la liste des poids
+        if ligne[3]!='': #Si la tache à un prérequis
+            pre_noeuds=ligne[3] #La liste des prerequis
+            for pre_noeud in pre_noeuds.split(): #Pour chaque prérequis
+                arcs.add((pre_noeud, noeud)) #On ajoute l'arc prerequis -> noeud
+        if noeud=='F': #Si la tache est la tache finale
+            poids_final=duree_tache #Alors on met en évidence le poids final
         
     return noeuds, arcs, poids, poids_final
 
 def conversion_unite(duree_tache):
     """Fonction qui convertie un str d'une duree temporelle et qui le convertir en int qui correspond au nombre de jour 
     que représente cette duree
-
+    La fonction peut raise une UniteError si l'unite de la duree n'est pas connue
     Args:
         duree_tache (str): la duree temporelle avec les unités (mois/annee/semaine)
 
     Returns:
         int: le nombre de jour qui correspond a la duree
     """
-    valeur=float(duree_tache.split()[0])
-    unite=duree_tache.split()[1]
-    if unite=='mois':
-        valeur*=30
-    elif unite=='annees' or unite=='annee':
-        valeur*=365
-    elif unite=='semaines' or unite=='semaine':
-        valeur*=7
-    elif unite=='jours' or unite=='jour':
-        valeur=valeur
-    else:
-        raise UniteError
+    valeur=float(duree_tache.split()[0]) #La valeur est le premier terme de la duree 
+    unite=duree_tache.split()[1] #L'unité est le second terme de la duree 
+    if unite=='mois': #Si l'unité est en mois
+        valeur*=30 #Alors on multiplie par 30 la valeur car il y a 30 jour dans un mois 
+    elif unite=='annees' or unite=='annee':  #Si l'unité est en annee
+        valeur*=365 #Alors on multiplie par 365 la valeur car il y a 365 jours dans un mois 
+    elif unite=='semaines' or unite=='semaine': #Si l'unité est en semaine
+        valeur*=7  #Alors on multiplie par 7 la valeur car il y a 7 jours dans un mois 
+    elif unite=='jours' or unite=='jour': #Si l'unite est en jour
+        valeur=valeur #Alors on ne change rien
+    else: #Sinon 
+        raise UniteError #On soulève une erreur d'unite
     return valeur
 
 
@@ -89,19 +90,22 @@ def ponderation_branches(arcs, poids)->set:
 
     Args:
         arcs (set): l'ensemble des branches
-        poids (_type_): l'ensembles des poids
+        poids (list): la liste des poids
+            tuples:
+                str: Le nom du noeud
+                int: Le poid du noeud
     
     Returns:
         set: l'ensemble des arcs ponderees
     """
-    arcs_ponderee=set()
-    for arc in arcs:
-        pre_noeud=arc[0]
-        for p in poids:
-            if pre_noeud==p[0]:
-                poids_branche=p[1]
-        arcs_ponderee.add((arc[0], arc[1], poids_branche))
-    return arcs_ponderee 
+    arcs_ponderes=set() #L'ensemble des arcs ponderees
+    for arc in arcs: #Pour chaque arcs
+        pre_noeud=arc[0] #Le noeud iitial de l'arc
+        for p in poids: #Pour chaque poids
+            if pre_noeud==p[0]: #Si le noeud initial de l'arc est le meme que ce noeud ponderee
+                poids_arc=p[1] #Alors le poids de de cet arc est egal au poids du noeud initial
+        arcs_ponderes.add((arc[0], arc[1], poids_arc)) #On rajoute au set des arcs ponderres l'arc avec son poids 
+    return arcs_ponderes
 
 def csv_to_graph(nom_fichier_csv:str):
     """Fonction qui convertie un fichier CSV en graphe pondérée
@@ -110,13 +114,17 @@ def csv_to_graph(nom_fichier_csv:str):
         nom_fichier_csv (str): Nom du fichier à convertir
 
     Returns:
-        DiGraphe: Graphe pondérée
+        list: les graphes pour chaque suivi
+            tuple:
+                list: la liste des noeuds
+                set: l'ensemble des arcs ponderres
+                int: le poids final de la tache qui n'est donc sur aucun arc
     """
-    graphs=[]
-    for n_suivi in [None,0,1,2]:
-        liste_csv=from_csv(nom_fichier_csv)
-        liste_csv.pop(0)
-        noeuds, arcs, poids, poids_final= traitement_information(liste_csv,n_suivi)
-        arcs_ponderee=ponderation_branches(arcs, poids)
-        graphs.append((noeuds,arcs_ponderee, poids_final))
+    graphs=[] #La liste des graphes
+    for n_suivi in [None,0,1,2]: #Pour chaque suivi
+        liste_csv=from_csv(nom_fichier_csv) #On converit le fichier en liste
+        liste_csv.pop(0) #On en lève le premier terme de la liste qui correspond au indication sur le fichier
+        noeuds, arcs, poids, poids_final= traitement_information(liste_csv,n_suivi) #On reccupère les différentes information
+        arcs_ponderee=ponderation_branches(arcs, poids) #On créer les arcs ponderes à partir des arcs et des poids
+        graphs.append((noeuds,arcs_ponderee, poids_final))  #On créer un tuple aves toutes les informations necessaires à créer un graphes ponderee 
     return graphs
