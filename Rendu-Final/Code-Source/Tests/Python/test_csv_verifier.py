8c2c89fd68f79728cbe9b5492006c5b68c9c38ad
Emilien8112 <emiliengrasset81@gmail.com>
2023-11-06 10:27:16 +0100

    test verif

--- Rendu-Final/Code-Source/Tests/Python/test_csv_verifier.py
+++ Rendu-Final/Code-Source/Tests/Python/test_csv_verifier.py
@@ -1,2 +1,15 @@
 import pytest
 from csv_verifier import*
+
+def test_fichier_correct():
+    assert verification_format_fichier("test_cool")==True,"Fichier valide"
+    assert verification_format_fichier("test_cool")==True,"Fichier valide"
+    
+    
+def test_fichier_incorrect():
+    assert verification_format_fichier("test_4_suivi")==False,"Le fichier ne comporte pas les colonnes nécéssaires"
+    assert verification_format_fichier("test_2taches_2")==False,'Il y a 2 fois la même tâche 2 aux lignes 4 et 5'
+    assert verification_format_fichier("test_2taches_finale")==False,"Cette organisation prévoit plusieurs tâches finales"
+    assert verification_format_fichier("test_0tache_finale")==False,"Cette organisation ne prévoit pas de tache finale"
+    assert verification_format_fichier("test_pas_de_durée")==False,"La tâche D Tache de début n'a pas de durée indiquée à la ligne 2"
+    assert verification_format_fichier("test_erreur_unité")==False, "Unité erronée à la ligne 2 dans la colonne 3"
