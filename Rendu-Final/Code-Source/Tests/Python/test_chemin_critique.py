582f31862c38368e82c89755bbe30922c0bd91d8
Concombrero <boyer.tim.travail@gmail.com>
2023-11-07 22:07:15 +0100

    test_chemin_critique

--- Rendu-Final/Code-Source/Tests/Python/test_chemin_critique.py
+++ Rendu-Final/Code-Source/Tests/Python/test_chemin_critique.py
@@ -4,4 +4,9 @@ from csv_to_graphe import*
 from cycle_detector import*
 from chemins_critiques import*
 from graphe_to_latex import*
-#A faire
+
+def test_chemin_critique():
+    g=csv_to_graph("test_chemin_critique_1")
+    assert ([0, 2, 3],4)==chemin_critique(g, 0 ,len(g.noeuds)-1)
+    g=csv_to_graph("test_chemin_critique_2")
+    assert ([0, 1, 3, 5],12)==chemin_critique(g, 0 ,len(g.noeuds)-1)
